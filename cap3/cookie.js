5bcbf7bdd2cb593df02e32cc0c009f53c43de2a5
Demy <18911887579@sina.cn>
2016-06-23 17:48:45 +0800

    update cap3
    Rock.html和Rock2.html区别文档开头是否有<!DOCTYPE html>，决定图片是否跟随浏览器窗口自适应大小

--- cap3/cookie.js
+++ cap3/cookie.js
@@ -4,12 +4,15 @@
 
 function writeCookie(name,value,days){
     //days为有效日期，表示cookie可以存在的天数
+    var expires = '';
     if(days){
         var date = new Date();
         date.setTime(date.getTime()+(days * 24 * 60 *60 * 1000));//将天数换算成毫秒数，再把毫秒数加到当前时间上
         //expires = "; expires=" + date.toGMTString();//toGMTString()函数用于以字符串的形式返回一个日期
         expires = "; expires=" + date.toUTCString();//toUTCString()函数用于以字符串的形式返回一个日期
     }
+    document.cookie = name + " = " + value +expires + ";path=/";
+
 }
 function readCookie(name){
     var searchName = name + "=";
